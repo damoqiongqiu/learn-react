b68720a2141bbf9fb5113c1ee4ce6ca3612200f3
damoqiongqiu <damoqiongqiu@126.com>
2018-12-05 18:13:26 +0800

    finish props example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import TestLifecycle from "./components/TestLifecycle";
+import Father from "./components/Father";
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <TestLifecycle></TestLifecycle>
+        <Father></Father>
       </div>
     );
   }
