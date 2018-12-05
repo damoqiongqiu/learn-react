a45f12cb1d4cae0fd69bc50582b8df48d33453cd
damoqiongqiu <damoqiongqiu@126.com>
2018-12-05 21:02:14 +0800

    finish hoc example

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
 import './App.css';
-
-/**
- * 纯函数无状态组件
- * @param {*} props 
- */
-function MyButton(props) {
-  return (
-    <button className="btn btn-danger">我的按钮</button>
-  );
-}
+import MyPanel from './components/MyPanel';
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="panel panel-primary">
-          <div className="panel-heading">面板标题</div>
-          <div className="panel-body">
-            <MyButton></MyButton>
-          </div>
-          <div className="panel-footer"></div>
-        </div>
+        <MyPanel></MyPanel>
       </div>
     );
   }
