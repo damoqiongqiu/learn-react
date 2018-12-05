fded25cbd7c49a375a24cfe8d7264d7c9cef649b
damoqiongqiu <damoqiongqiu@126.com>
2018-12-05 16:59:17 +0800

    finish lifecycle examples

---
+++ src/components/TestLifecycle.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+/**
+ * 共9个生命周期事件
+ * 1、构造
+ * 2、mount
+ * 3、update
+ * 4、unmount
+ */
+class TestLifecycle extends Component {
+    constructor() {
+        super();
+        console.log("TestLifecycle constructor...");
+    }
+
+    componentWillMount() {
+        console.log("TestLifecycle componentWillMount...");
+    }
+
+    componentDidMount() {
+        console.log("TestLifecycle componentDidMount...");
+    }
+
+    componentWillReceiveProps() {
+        console.log("TestLifecycle componentWillReceiveProps...");
+    }
+
+    componentWillUpdate() {
+        console.log("TestLifecycle componentWillUpdate...");
+    }
+
+    componentDidUpdate() {
+        console.log("TestLifecycle componentDidUpdate...");
+    }
+
+    shouldComponentUpdate() {
+        console.log("TestLifecycle shouldComponentUpdate...");
+    }
+
+    render() {
+        console.log("TestLifecycle render...");
+        return (
+            <div className="container-fluid">
+                <div className="panel panel-primary">
+                    <div className="panel-heading">TestLifecycle组件</div>
+                    <div className="panel-body">
+                        <div className="list-group">
+                            <a href="#" className="list-group-item disabled">
+                                Cras justo odio
+                            </a>
+                            <a href="#" className="list-group-item">Dapibus ac facilisis in</a>
+                            <a href="#" className="list-group-item">Morbi leo risus</a>
+                            <a href="#" className="list-group-item">Porta ac consectetur ac</a>
+                            <a href="#" className="list-group-item">Vestibulum at eros</a>
+                        </div>
+                    </div>
+                    <div className="panel-footer"></div>
+                </div>
+            </div>
+        );
+    }
+
+    componentWillUnmount() {
+        console.log("TestLifecycle componentWillUnmount...");
+    }
+}
+
+export default TestLifecycle;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
