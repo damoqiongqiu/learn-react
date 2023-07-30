196e68c26f18b35359441cf51752dd491e4a85c9
大漠穷秋 <damoqiongqiu@126.com>
2023-07-30 22:20:00 +0800

    加注释

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactClock from './components/react-clock';
 import './App.css';
 
+/**
+ * Step1: 直接在 App 组件中编写时钟代码
+ * Step2: 把时钟代码抽成一个组件
+ * @returns 
+ */
 function App() {
   return (
     <div className="App">
