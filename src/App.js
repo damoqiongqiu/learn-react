2af3e683b85ffb1d25d26613c3472788dff5d8d4
大漠穷秋 <damoqiongqiu@126.com>
2023-08-01 13:13:48 +0800

    修改案例代码

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+/**
+ * 本节目标：上手体验 JSX/useEffect/useState 
+ * step1: 在 HTML 标签中获取 time
+ * step2: 用 useEffect 实现定时器
+ * step3: 修改外观
+ * step4: 把时钟逻辑抽离成一个组件
+ * 完成以上步骤，就完成了 Quick Start 这个小节的目标。
+ * @returns 
+ */
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Clock</h1>
+      <h2>{time.toLocaleString()}.</h2>
     </div>
   );
 }
