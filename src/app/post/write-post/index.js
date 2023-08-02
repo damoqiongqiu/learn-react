58f03498c38615e9751d0394aafd73ffc708a456
大漠穷秋 <damoqiongqiu@126.com>
2023-08-02 19:39:01 +0800

    添加注释

--- src/app/post/write-post/index.js
+++ src/app/post/write-post/index.js
@@ -1,30 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 /**
- * 受控表单案例
- * “受控”的含义是：表单中的输入项数据完全交给 React 的 state 机制来管理。
- * @param {*} props 
- * @returns 
+ * 表单校验案例
  */
 function WritePost(props) {
-    //受控表单必须提供初始值，否则 React 会报警告，因为 undefined 会导致 React 无法确定组件到底是受控还是非受控组件。
-    //@see https://react.dev/reference/react-dom/components/input
-    const [postDetail, setPostDetail] = React.useState({
+    const [postDetail, setPostDetail] = useState({
         title: '',
         content: '',
         isOriginal: true,
-        tag: ''
+        tag: '',
     });
 
-    //React 要求受控表单必须提供 onChange 事件处理函数，如果不提供，React 会把表单设置为只读状态。
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setPostDetail({
-            ...postDetail,
-            [name]: value
-        });
-    }
+    const [errors, setErrors] = useState({
+        title: '',
+        content: '',
+        tag: '',
+    });
 
     const handleSubmit = (e) => {
         //阻止浏览器默认的跳转行为
@@ -43,57 +34,106 @@ function WritePost(props) {
         console.log(formJson);
 
         //接下来就可以提交到服务器了，在下一节中我们会介绍如何使用 axios 发送请求。
-    }
+    };
+
+    // 表单校验函数
+    const validateForm = () => {
+        let valid = true;
+        const newErrors = {
+            title: '',
+            content: '',
+            tag: '',
+        };
+
+        // 校验标题长度是否在2到32个字符之间
+        if (postDetail.title.trim().length < 2 || postDetail.title.trim().length > 32) {
+            newErrors.title = '标题长度应在2到32个字符之间';
+            valid = false;
+        }
+
+        // 校验内容长度是否在10到200个字符之间
+        if (postDetail.content.trim().length < 10 || postDetail.content.trim().length > 200) {
+            newErrors.content = '内容长度应在10到200个字符之间';
+            valid = false;
+        }
+
+        // 校验标签长度是否在2到32个字符之间
+        if (postDetail.tag.trim().length < 2 || postDetail.tag.trim().length > 32) {
+            newErrors.tag = '标签长度应在2到32个字符之间';
+            valid = false;
+        }
+
+        // 更新错误状态
+        setErrors(newErrors);
+        return valid;
+    };
+
+    // 表单输入变更处理函数
+    const handleChange = (event) => {
+        const { name, value, type, checked } = event.target;
+        // 处理 checkbox 输入项
+        const newValue = type === 'checkbox' ? checked : value;
+        setPostDetail({
+            ...postDetail,
+            [name]: newValue,
+        });
+    };
 
     return (
         <div className="row">
             <div className="col-md-12">
-                <form noValidate onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="title">标题</label>
+                <form onSubmit={handleSubmit}>
+                    <div className={`form-group ${errors.title ? "has-error" : ""}`} >
+                        <label>标题：</label>
                         <input
                             type="text"
-                            className="form-control"
-                            placeholder="请输入标题"
                             name="title"
                             value={postDetail.title}
                             onChange={handleChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="content">内容</label>
-                        <textarea
                             className="form-control"
-                            rows="10"
-                            placeholder="请输入内容"
+                        />
+                        {errors.title && <span className="text-danger">{errors.title}</span>}
+                    </div>
+
+                    <div className={`form-group ${errors.content ? "has-error" : ""}`} >
+                        <label>内容：</label>
+                        <textarea
                             name="content"
+                            rows="10"
                             value={postDetail.content}
                             onChange={handleChange}
-                        ></textarea>
+                            className="form-control"
+                        />
+                        {errors.content && <span className="text-danger">{errors.content}</span>}
                     </div>
+
                     <div className="checkbox">
                         <label>
                             <input
                                 type="checkbox"
-                                defaultChecked={true}//注意 React 中 default*** 这种写法，与 HTML 标准不同。
                                 name="isOriginal"
-                                value={postDetail.isOriginal}
+                                checked={postDetail.isOriginal}
                                 onChange={handleChange}
-                            />原创
+                            />{' '}
+                            原创
                         </label>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="tag">标签</label>
+
+                    <div className={`form-group ${errors.tag ? "has-error" : ""}`} >
+                        <label>标签：</label>
                         <input
                             type="text"
-                            className="form-control"
-                            placeholder="请输入标签"
                             name="tag"
                             value={postDetail.tag}
                             onChange={handleChange}
+                            className="form-control"
                         />
+                        {errors.tag && <span className="text-danger">{errors.tag}</span>}
                     </div>
-                    <button type="submit" className="btn btn-success">提交</button>
+
+                    <button type="submit" className="btn btn-success" onClick={validateForm}>
+                        提交
+                    </button>
                 </form>
             </div>
         </div>
