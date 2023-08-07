c7cd6f404bdf0aa430dad8e04fed8733268da69e
大漠穷秋 <damoqiongqiu@126.com>
2023-08-07 22:54:36 +0800

    添加 Jest 工具，添加测试用例

--- __jest__/__tests__/utils/suite1.js
+++
@@ -1,5 +0,0 @@
-import add from '../../../src/common/utils/math'
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
-});
--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,9 @@
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.queryByText(/管理/i);
   expect(linkElement).toBeInTheDocument();
 });
