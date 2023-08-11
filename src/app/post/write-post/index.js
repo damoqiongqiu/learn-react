864fafdcdcf1a25eb889b1c538d5ca4ea5c14006
大漠穷秋 <damoqiongqiu@126.com>
2023-08-11 19:19:19 +0800

    refactor code

--- src/app/post/write-post/index.js
+++ src/app/post/write-post/index.js
@@ -2,6 +2,53 @@ import React, { useState } from "react";
 import Ajv from 'ajv';
 import ajvErrors from 'ajv-errors';
 
+// 表单输入项数据规格定义
+const schema = {
+    "type": "object",
+    "properties": {
+        "title": {
+            "type": "string",
+            "minLength": 1,
+            "maxLength": 32,
+            "errorMessage": "标题长度在 1 到 32 个字符之间。"
+        },
+        "content": {
+            "type": "string",
+            "minLength": 10,
+            "maxLength": 200,
+            "errorMessage": "内容长度在 10 到 200 个字符之间。"
+        },
+        "isOriginal": {
+            "type": "boolean"
+        },
+        "tag": {
+            "tagValidator": true,
+            "errorMessage": "标签长度在 2 到 12 个字符之间。",
+        }
+    },
+    "required": ["title", "content"]
+}
+const ajv = new Ajv({ allErrors: true });
+// 添加 ajv-errors 插件
+ajvErrors(ajv);
+//tag 字段的校验规则比较复杂：当用户输入了 tag 字段时，才进行校验；如果用户没有输入 tag 字段的内容，请不要强制校验，因为 tag 不是必填字段。
+//这里需要自定义一个校验函数
+ajv.addKeyword('tagValidator', {
+    validate: function (schema, data) {
+        if (data.trim() === '') {
+            return true;
+        }
+        let temp = data.trim();
+        if (temp.length >= 2 && temp.length <= 12) {
+            return true;
+        }
+        return false;
+    },
+    errors: false
+});
+//compile 方法返回一个校验函数， compile 的速度比较慢，所以我们只需要在组件初始化时调用一次即可。
+const validate = ajv.compile(schema);
+
 /**
  * ajv.js 表单校验案例
  */
@@ -12,52 +59,7 @@ function WritePost(props) {
         isOriginal: true,
         tag: '',
     });
-    // 表单输入项数据规格定义
-    const schema = {
-        "type": "object",
-        "properties": {
-            "title": {
-                "type": "string",
-                "minLength": 1,
-                "maxLength": 32,
-                "errorMessage": "标题长度在 1 到 32 个字符之间。"
-            },
-            "content": {
-                "type": "string",
-                "minLength": 10,
-                "maxLength": 200,
-                "errorMessage": "内容长度在 10 到 200 个字符之间。"
-            },
-            "isOriginal": {
-                "type": "boolean"
-            },
-            "tag": {
-                "tagValidator": true,
-                "errorMessage": "标签长度在 2 到 12 个字符之间。",
-            }
-        },
-        "required": ["title", "content"]
-    }
     const [errors, setErrors] = useState({});
-    const ajv = new Ajv({ allErrors: true });
-    // 添加 ajv-errors 插件
-    ajvErrors(ajv);
-    //tag 字段的校验规则比较复杂：当用户输入了 tag 字段时，才进行校验；如果用户没有输入 tag 字段的内容，请不要强制校验，因为 tag 不是必填字段。
-    //这里需要自定义一个校验函数
-    ajv.addKeyword('tagValidator', {
-        validate: function (schema, data) {
-            if (data.trim() === '') {
-                return true;
-            }
-            let temp = data.trim();
-            if (temp.length >= 2 && temp.length <= 12) {
-                return true;
-            }
-            return false;
-        },
-        errors: false
-    });
-    const validate = ajv.compile(schema);
 
     // 表单输入变更处理函数
     const handleChange = (event) => {
