b5ad19d6b9a25b3d661d5abcbda0f7757bccb7da
大漠穷秋 <damoqiongqiu@126.com>
2023-08-12 20:03:08 +0800

    添加注释

--- src/app/App.js
+++ src/app/App.js
@@ -6,11 +6,13 @@ import NavBar from './components/nav-bar';
 import './App.scss';
 
 function App() {
+  //目前这里没有业务逻辑
   return (
     <>
       <NavBar></NavBar>
       <div className="container-fluid">
         <div className='main-area'>
+          {/* row-column 这个行列布局工具样式，是 Bootstrap 里面使用频率最高的工具样式 */}
           <div className="row">
             <div className="col-md-9">
               <PostList page={1}></PostList>
--- src/app/components/nav-bar/index.js
+++ src/app/components/nav-bar/index.js
@@ -3,6 +3,15 @@ import "./index.scss";
 
 function NavBar(props) {
     return (
+        // nav 是 H5 引入的新标签，专门用来表示导航条
+        // navbar 是 Bootstrap 的导航条组件
+        // navbar-default 是 Bootstrap 导航条组件的默认样式
+        // navbar-fixed-top 是 Bootstrap 导航条组件的固定在顶部的样式
+        // container-fluid 是 Bootstrap 的容器组件，表示宽度为 100% 的容器
+        // navbar-header 是 Bootstrap 导航条组件的头部样式
+        // navbar-brand 是 Bootstrap 导航条组件的品牌样式
+        // navbar-toggle 是 Bootstrap 导航条组件的切换按钮样式
+        // collapsed 是 Bootstrap 用来支持响应式的样式，在移动端访问时，导航条会自动折叠
         <nav className="navbar navbar-default navbar-fixed-top">
             <div className="container-fluid">
                 <div className="navbar-header">
