ddc1d2a355a7d65688d5e05f42ac61d9368c8d4b
大漠穷秋 <damoqiongqiu@126.com>
2023-08-12 19:13:47 +0800

    添加示例和注释

--- src/app/App.js
+++ src/app/App.js
@@ -7,17 +7,24 @@ function App() {
   return (
     <div className="container-fluid">
       <div className='App'>
+
         <div className="row">
           <div className="col-md-9">
             <div className="jumbotron">
               <h1>头条文章</h1>
+              <p>这是文章的内容。这是文章的内容。这是文章的内容。这是文章的内容。</p>
             </div>
             {/* 可以通过 props 给组件传递任意参数，这是一个极其强大的机制 */}
-            <PostList page={1}></PostList>
+            <PostList
+              page={1}
+              userInfo={{ userName: "大漠穷秋", age: 18 }}
+              roleId={10010}
+            ></PostList>
           </div>
           <div className="col-md-3">
           </div>
         </div>
+
       </div>
     </div>
   );
--- src/app/components/post-list/index.js
+++ src/app/components/post-list/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import postListMock from "./post-list.mock.json";
 
+//每一个 React 组件的构造函数，默认都有一个 props 参数
 function PostList(props) {
     //props 是组件的属性，是只读的，不能修改
     console.log(props);
+
+    const { page, userInfo, roleId } = props;
+    console.log(page);
+    console.log(userInfo);
+    console.log(roleId);
 
     //state 是组件的状态，是可读可写的
     const [postList, setPostList] = useState([]);
@@ -14,19 +20,38 @@ function PostList(props) {
         }, 1000);
     }, []);
 
+    const loadMore = () => {
+        console.log("加载更多");
+        for (let i = 0; i < 100; i++) {
+            postList.push({
+                id: 100 + i,
+                title: "这是一篇新文章",
+                updateTime: new Date().toLocaleString()
+            });
+        }
+        setPostList([...postList]);
+    }
+
     return (
-        <ul className="list-group">
-            {
-                postList.map(post => (
-                    <li className="list-group-item" key={post.id}>
-                        <div className="d-flex justify-content-between">
-                            <span>{post.title}</span>
-                            <span>{post.updateTime}</span>
-                        </div>
-                    </li>
-                ))
-            }
-        </ul>
+        <>
+            {/* ul 是 HTML 里面的无序列表标签 */}
+            <ul className="list-group">
+                {
+                    postList.map(post => (
+                        //li 是 HTML 里面的列表项标签
+                        <li className="list-group-item" key={post.id}>
+                            <div className="d-flex justify-content-between">
+                                <span>{post.title}</span>
+                                <span>{post.updateTime}</span>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+            <button className="btn btn-primary" onClick={() => {
+                loadMore();
+            }}>加载更多</button>
+        </>
     );
 }
 
